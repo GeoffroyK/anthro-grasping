275d8c9d3f2abcca9c716bcf6a37b0abfd359d02
GeoffroyK <geoffroy.keime@gmail.com>
2023-07-27 15:36:50 +0200

    add scripts to launch full training batch

--- cornell_training.sh
+++ cornell_training.sh
@@ -1,19 +1,8 @@
 #!/bin/bash
 
-python main.py \
-    --dataset-path /media/pollen/T7/cornell \
-    --dataset cornell \
-    --use-depth 1 \
-    --use-rgb 1 \
-    --split 0.9 \
-    --ds-rotate 0.0 \
-    --num-workers 8 \
-    --batch-size 32 \
-    --vis False \
-    --epochs 1000 \
-    --batches-per-epoch 200\
-    --val-batches 32 \
-    --description "Learning on the cornell dataset with 10% of the dataset removed for cross validation" \
-    --outdir output/cornell_2 \
-    --logdir cornell_2
-    
+# Train RGB only
+sh ./cornell_training_rgb_only.sh
+# Train depth only
+sh ./cornell_training_rgb_only.sh
+# Train RGB-D 
+sh ./cornell_training_rgbd.sh
---
+++ cornell_training_rgbd.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+python main.py \
+    --dataset-path /media/pollen/T7/cornell \
+    --dataset cornell \
+    --use-depth 1 \
+    --use-rgb 1 \
+    --split 0.9 \
+    --ds-rotate 0.1 \
+    --num-workers 8 \
+    --batch-size 32 \
+    --vis False \
+    --epochs 250 \
+    --batches-per-epoch 200\
+    --val-batches 32 \
+    --description "Learning on the cornell dataset with 10% of the dataset removed for cross validation" \
+    --outdir output/cornell_2 \
+    --logdir cornell_2
+    
